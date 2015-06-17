8aabc58058a658fd7eb3c35a197ac907c5728298
shuhaida6704 <shuhaida@tmsk.uitm.edu.my>
2015-06-17 16:25:28 +0800

    assignment2_Shuhaida

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,11 @@
 
+## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). 
+##The assignment is to write a pair of functions that cache the inverse of a matrix.
+
+##nameAuthor: Shuhaida Shuhidan
+##todo::makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+##todo::cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), then the cachesolve should retrieve the inverse from the cache.
+
 makeCacheMatrix <- function(x = matrix()) {
   ##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
